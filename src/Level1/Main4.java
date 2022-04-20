646d7319db786c5c58753c18a3c5733a927de812
Suhyang Gwon <znsl65@naver.com>
2022-04-20 19:45:56 +0900

    [update] 2022.04.20

--- src/Level1/Main4.java
+++ src/Level1/Main4.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class Main4 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int 
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        sc.close();
+	        
+	        int c = a*(b%10);
+	        int d = a*((b%100) / 10);
+	        int e = a*(b/100);
+	        System.out.println(c);
+	        System.out.println(d);
+	        System.out.println(e);
+	        System.out.println(c+ (d*10) + (e*100));
 	}
 }
